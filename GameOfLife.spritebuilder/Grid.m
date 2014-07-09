1a560e17861124ac8d3706ff4fed039ae025e511
judy mckelvey <judy@judys-MacBook.local>
2014-07-09 11:08:58 -0700

    Automatically Committed for MakeGamesWithUs

--- GameOfLife.spritebuilder/Grid.m
+++ GameOfLife.spritebuilder/Grid.m
@@ -146,11 +146,13 @@ static const int GRID_COLUMNS = 10;
             Creature *currentCreature = _gridArray[i][j];
             if(currentCreature.livingNeighbors == 3){
                 currentCreature.isAlive = TRUE;
-                numAlive++;
             }else{
                 if(currentCreature.livingNeighbors <= 1 || currentCreature.livingNeighbors >= 4){
                     currentCreature.isAlive = FALSE;
                 }
+            }
+            if(currentCreature.isAlive){
+                numAlive++;
             }
         }
     }
